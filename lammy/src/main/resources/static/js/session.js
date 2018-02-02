6d64afc55391020610051ee41b132627be1e555d
orangewest <jinpengcn@foxmail.com>
2018-02-02 13:26:27 +0800

    t

--- lammy/src/main/resources/static/js/session.js
+++ lammy/src/main/resources/static/js/session.js
@@ -1,20 +1,16 @@
 $(function () {
 	var userId = layui.sessionData('user').userId;
-	if(isEmpty(userId)){
-		location.href = '/lammy/login.html';
-	}else{
-		$.axget('/user',null,function(result){
-	    	if(result.code==0){
-				layui.sessionData('user', {
-					key: 'username',
-					value: result.data.username
-				});
-				layui.sessionData('user', {
-					key: 'userId',
-					value: result.data.id
-				});
-			}
-	    })
-	}
+	$.axget('/user',null,function(result){
+    	if(result.code==0){
+			layui.sessionData('user', {
+				key: 'username',
+				value: result.data.username
+			});
+			layui.sessionData('user', {
+				key: 'userId',
+				value: result.data.id
+			});
+		}
+    })
 	
 })
